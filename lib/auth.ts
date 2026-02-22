9d1d53baf68c262f1b01a14157502f29b16e7291
NR NATION <horizonthesphare@gmail.com>
2026-02-22 13:05:09 +0530

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,34 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "./prisma";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password)
+          throw new Error("Missing credentials");
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email }
+        });
+
+        if (!user) throw new Error("User not found");
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) throw new Error("Invalid password");
+
+        return user;
+      }
+    })
+  ],
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET
+};
